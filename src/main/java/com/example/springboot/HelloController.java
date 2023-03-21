78fa05fe7c1d38b2dc771e04ed0d7e96df18be33
haewons-tanzu <haewons@vmware.com>
2023-03-21 10:48:13 +0900

    11

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.springboot;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String str = "Greetings from Spring Boot + Tanzu!";
+		String str = "Greetings from Spring Boot + TanzuQQQQ!";
 		return str;
 	}
 
