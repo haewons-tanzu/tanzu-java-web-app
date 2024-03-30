e67e8155ff67c1b891343c085b8a148e6f82e05d
Haewon Shin <22441424+haewons-tanzu@users.noreply.github.com>
2024-03-31 07:51:22 +0900

    Update Application.java

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -9,9 +9,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+//public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
