82c6cf4f4f37e9f45969eb98c1929ff022e3a456
Haewon Shin <22441424+haewons-tanzu@users.noreply.github.com>
2024-03-31 01:33:44 +0900

    Create TomcatConfig.java

---
+++ src/main/java/com/example/springboot/TomcatConfig.java
@@ -0,0 +1,28 @@
+/*
+ * Tomcat Server Config
+ */
+@Configuration
+public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createAjpConnector());
+		return tomcat;
+	}
+
+	private Connector createAjpConnector() {
+		Connector ajpConnector = new Connector("AJP/1.3");
+		ajpConnector.setPort(9999);	// ajp Port
+		ajpConnector.setSecure(false);
+		ajpConnector.setAllowTrace(false);
+		ajpConnector.setScheme("http");
+		ajpConnector.setRedirectPort(8080);	// http or https port
+		return ajpConnector;
+	}
+
+	@Override
+	public void customize(TomcatServletWebServerFactory factory) {
+		factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setAttribute("relaxedQueryChars", "|"));	
+	}
+}
