67fb0c7378b583e64c3cae0170dab4d0c36fc033
Haewon Shin <22441424+haewons-tanzu@users.noreply.github.com>
2024-03-31 07:20:08 +0900

    Update TomcatConfig.java

--- src/main/java/com/example/springboot/TomcatConfig.java
+++ src/main/java/com/example/springboot/TomcatConfig.java
@@ -1,38 +1,38 @@
-package com.example.springboot;
+// package com.example.springboot;
 
-import org.apache.catalina.connector.Connector;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.apache.catalina.connector.Connector;
+// import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+// import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+// import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+// import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-/*
- * Tomcat Server Config
- */
-@Configuration
-public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+// /*
+//  * Tomcat Server Config
+//  */
+// @Configuration
+// public class TomcatConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
-	@Bean
-	public ServletWebServerFactory servletContainer() {
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-		tomcat.addAdditionalTomcatConnectors(createHttpConnector());
-		return tomcat;
-	}
+// 	@Bean
+// 	public ServletWebServerFactory servletContainer() {
+// 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+// 		tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+// 		return tomcat;
+// 	}
 
-	private Connector createHttpConnector() {
-		Connector httpConnector = new Connector("HTTP/1.1");
-		httpConnector.setPort(8443);	// HTTP Port
-		httpConnector.setSecure(false);
-		httpConnector.setAllowTrace(false);
-		httpConnector.setScheme("http");
-		httpConnector.setRedirectPort(8080);	// http or https port
-		return httpConnector;
-	}
+// 	private Connector createHttpConnector() {
+// 		Connector httpConnector = new Connector("HTTP/1.1");
+// 		httpConnector.setPort(8443);	// HTTP Port
+// 		httpConnector.setSecure(false);
+// 		httpConnector.setAllowTrace(false);
+// 		httpConnector.setScheme("http");
+// 		httpConnector.setRedirectPort(8080);	// http or https port
+// 		return httpConnector;
+// 	}
 
-	@Override
-	public void customize(TomcatServletWebServerFactory factory) {
-		factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setAttribute("relaxedQueryChars", "|"));	
-	}
-}
+// 	@Override
+// 	public void customize(TomcatServletWebServerFactory factory) {
+// 		factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setAttribute("relaxedQueryChars", "|"));	
+// 	}
+// }
