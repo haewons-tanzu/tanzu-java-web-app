7693ca18877a439dc2416071aa4ebf280be6224c
Haewon Shin <22441424+haewons-tanzu@users.noreply.github.com>
2024-03-31 08:05:01 +0900

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,9 +3,7 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
@@ -14,16 +12,6 @@ public class HelloController {
 	public String index() {
 		String str = "Greetings from Spring Boot + Tanzu! Welcome!!!";
 		return str;
-	}
-
-	@GetMapping("/test")
-	public String getParamByName(@RequestParam String name){
-		return "getParamByName.Input Name: " + name;
-	}
-
-	@GetMapping("/test/{name}")
-	public String getRestParamByName(@PathVariable("name") String name){
-		return "getRestParamByName.Input Name: " + name;
 	}
 
 }
